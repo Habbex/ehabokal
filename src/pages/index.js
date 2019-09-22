0f1ed133dd0a24773cb52e3c6fa76275d9a88663
Habbex <ihabokal@hotmail.com>
2019-09-22 18:58:36 +0000

    Test

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,9 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Ehabs Portfolio</p>
+    <p>Currently bulding something great</p>
+    <img src="../images/gatsby-astronaut.png"/>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
